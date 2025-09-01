300cff2330a75dd71b1ded22061f8d28ddc9e4c3
Bo Bramer <bramer.bo@gmail.com>
2025-09-01 16:13:10 -0700

    loading state

--- src/components/devices/DeviceInfo.tsx
+++ src/components/devices/DeviceInfo.tsx
@@ -2,10 +2,11 @@
 
 import { Temperature } from '@/components/devices/Temperature';
 import { Card, CardContent, CardDescription, CardTitle } from '@/components/ui/card';
+import { Spinner } from '@/components/ui/spinner';
 import { useDeviceInfo } from '@/hooks/useDeviceInfo';
 
 export function DeviceInfo({ deviceId }: { deviceId: string }) {
-  const { deviceInfo, error, isLoading } = useDeviceInfo(deviceId);
+  const { deviceInfo, isLoading } = useDeviceInfo(deviceId);
 
   return (
     <Card className="p-4">
@@ -15,14 +16,18 @@ export function DeviceInfo({ deviceId }: { deviceId: string }) {
         <p>Model: {deviceInfo.model ?? '...'}</p>
         <p>Firmware Version: {deviceInfo.firmwareVersion ?? '...'}</p>
       </CardDescription>
-      {isLoading ? null : (
-        <CardContent className="p-0">
-          <p>
-            Temperature: <Temperature celsiusValue={deviceInfo.tempIndoor ?? 0} />
-          </p>
-          <p>Humidity: {deviceInfo.humIndoor}%</p>
-        </CardContent>
-      )}
+      <CardContent className="p-0">
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <p>
+              Internal Temperature: <Temperature celsiusValue={deviceInfo.tempIndoor ?? 0} />
+            </p>
+            <p>Internal Humidity: {deviceInfo.humIndoor}%</p>
+          </>
+        )}
+      </CardContent>
     </Card>
   );
 }
---
+++ src/components/ui/spinner.tsx
@@ -0,0 +1,5 @@
+import { RotatingLines } from 'react-loader-spinner';
+
+export function Spinner() {
+  return <RotatingLines width="32" strokeColor="grey" />;
+}
