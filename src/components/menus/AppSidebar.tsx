7cdce4c042d627ff9513194f603ef1adb2af50c9
Bo Bramer <bramer.bo@gmail.com>
2025-09-14 09:01:06 -0700

    chore: update version

--- src/components/menus/AppSidebar.tsx
+++ src/components/menus/AppSidebar.tsx
@@ -84,7 +84,7 @@ export function AppSidebar() {
             <SidebarMenuButton asChild>
               <Link href="https://github.com/wuguishifu/thermo-automation" target="_blank">
                 <Code />
-                <span>Version 0.0.1</span>
+                <span>Version 0.1.1</span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
